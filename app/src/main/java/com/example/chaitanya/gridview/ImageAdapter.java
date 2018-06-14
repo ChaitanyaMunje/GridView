f18addda881426ebb2ee1ecb87722500ac1b0484
Chaitanya <chaitanyamunje@gmail.com>
2018-06-14 16:53:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/chaitanya/gridview/ImageAdapter.java
@@ -0,0 +1,42 @@
+package com.example.chaitanya.gridview;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class ImageAdapter extends PagerAdapter {
+    private Context mContext;
+    private int [] mImages=new int[]{R.color.one,R.color.two,R.color.three,R.color.four};
+    ImageAdapter (Context context)
+    {
+        mContext=context;
+    }
+    @Override
+    public int getCount() {
+        return mImages.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view==object;
+    }
+    //press control and O
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        ImageView imageView=new ImageView(mContext);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setImageResource(mImages[position]);
+        container.addView(imageView,0);
+        return imageView;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((ImageView)object);
+    }
+}
---
+++ app/src/main/java/com/example/chaitanya/gridview/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.chaitanya.gridview;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ViewPager viewPager=findViewById(R.id.viewpager);
+        ImageAdapter adapter=new ImageAdapter(this);
+        viewPager.setAdapter(adapter);
+
+
+    }
+}
